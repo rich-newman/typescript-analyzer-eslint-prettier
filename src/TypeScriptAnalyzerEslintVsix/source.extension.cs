ba1ebd3ef3e21f68d1d09d668d55baedbed571b2
Rich Newman <rich-newman@noreply.github.com>
2022-05-17 12:07:32 +0100

    Give VS2017/9 version a different name for MarketPlace, update readme

--- src/TypeScriptAnalyzerEslintVsix/source.extension.cs
+++ src/TypeScriptAnalyzerEslintVsix/source.extension.cs
@@ -8,7 +8,7 @@ namespace TypeScriptAnalyzerEslintVsix
     internal sealed partial class Vsix
     {
         public const string Id = "45A5655D-42CD-4BB8-82B2-01D177E45B99";
-        public const string Name = "TypeScript Analyzer (ESLint, Prettier)";
+        public const string Name = "TypeScript Analyzer (ESLint, Prettier) 2019";
         public const string Description = @"Lints and formats JavaScript and TypeScript files directly in Visual Studio using ESLint and Prettier";
         public const string Language = "en-US";
         public const string Version = "2.0";
