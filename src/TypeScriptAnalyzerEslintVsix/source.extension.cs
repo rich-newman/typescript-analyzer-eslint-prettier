a1d8b30b29f2df6aa47177d4f28376d1c45e2ead
Rich Newman <rich-newman@noreply.github.com>
2022-06-10 20:00:59 +0100

    Revert VS2017/19 manifest to settings that should install in the experimental instance

--- src/TypeScriptAnalyzerEslintVsix/source.extension.cs
+++ src/TypeScriptAnalyzerEslintVsix/source.extension.cs
@@ -8,11 +8,11 @@ namespace TypeScriptAnalyzerEslintVsix
     internal sealed partial class Vsix
     {
         public const string Id = "45A5655D-42CD-4BB8-82B2-01D177E45B99";
-        public const string Name = "TypeScript Analyzer (ESLint, Prettier) 2019";
+        public const string Name = "TypeScript Analyzer (ESLint, Prettier)";
         public const string Description = @"Lints and formats JavaScript and TypeScript files directly in Visual Studio using ESLint and Prettier";
         public const string Language = "en-US";
         public const string Version = "2.1";
-        public const string Author = "Rich Newman";
+        public const string Author = "RN";
         public const string Tags = "Lint,ESLint,Prettier,JavaScript,TypeScript";
     }
 }
