6f59dc48eeed81cff53d13e4a6df81972a1e7ed4
Rich Newman <rich-newman@noreply.github.com>
2023-01-12 15:25:09 +0000

    Update to latest npm packages, fix test that could work by accident previously

--- src/TypeScriptAnalyzerEslintTestShared/artifacts/localconfig/.eslintrc.js
+++ src/TypeScriptAnalyzerEslintTestShared/artifacts/localconfig/.eslintrc.js
@@ -1,12 +1,8 @@
 const config = {
-	"parser": "@typescript-eslint/parser",
     "parserOptions": {
       "ecmaVersion": "latest",
       "sourceType": "module"
-    },
-    "plugins": [
-      "@typescript-eslint"
-    ]
+    }
 };
 
 module.exports = config;
